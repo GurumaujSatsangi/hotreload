d0d7b0de8702d41e17488ceca2bb6823c79d97af
Gurumauj Satsangi <gurumaujsatsangi@gmail.com>
2026-03-08 11:17:01 +0530

    Add recursive directory watching and dynamic folder detection

--- internal/watcher/watcher.go
+++ internal/watcher/watcher.go
@@ -1,6 +1,12 @@
 package watcher
 
-import "github.com/fsnotify/fsnotify"
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+
+	"github.com/fsnotify/fsnotify"
+)
 
 type Watcher struct {
 	watcher *fsnotify.Watcher
@@ -13,7 +19,7 @@ func NewWatcher(root string) (*Watcher, error) {
 		return nil, err
 	}
 
-	if err := w.Add(root); err != nil {
+	if err := addDirectoryTree(w, root); err != nil {
 		_ = w.Close()
 		return nil, err
 	}
@@ -31,6 +37,20 @@ func NewWatcher(root string) (*Watcher, error) {
 					close(instance.events)
 					return
 				}
+
+				if event.Op&fsnotify.Create != 0 {
+					info, err := os.Stat(event.Name)
+					if err == nil && info.IsDir() {
+						_ = addDirectoryTree(w, event.Name)
+					}
+				}
+
+				if event.Op&(fsnotify.Remove|fsnotify.Rename) != 0 {
+					if _, err := os.Stat(event.Name); err != nil {
+						_ = w.Remove(event.Name)
+					}
+				}
+
 				if event.Op&(fsnotify.Write|fsnotify.Create|fsnotify.Remove) != 0 {
 					instance.events <- event.Name
 				}
@@ -48,4 +68,22 @@ func NewWatcher(root string) (*Watcher, error) {
 
 func (w *Watcher) Events() <-chan string {
 	return w.events
+}
+
+func addDirectoryTree(w *fsnotify.Watcher, root string) error {
+	return filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return nil
+		}
+		if !d.IsDir() {
+			return nil
+		}
+		if addErr := w.Add(path); addErr != nil {
+			if os.IsNotExist(addErr) {
+				return nil
+			}
+			return addErr
+		}
+		return nil
+	})
 }
